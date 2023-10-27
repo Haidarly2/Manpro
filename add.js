24b6c4993ce44e69d7a0dc4b23773e7b7f28d2f8
Haidarly2 <haidar.aly007@gmail.com>
2023-10-27 12:52:09 +0700

    First Commit

---
+++ add.js
@@ -0,0 +1,11 @@
+// var div = document.getElementById('after');
+// var display = 0;
+
+function hideShow(x) {
+    if (x == 0) {
+        document.getElementById("after").style.display = "block";
+    } else {
+        document.getElementById("after").style.display = "none";
+    }
+    return;
+}
